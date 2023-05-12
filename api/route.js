b5277a541f93d2c956800db1b52254287b89eca6
unknown <owolewaonimisi@gmail.com>
2023-05-12 01:56:35 -0700

    we love this

--- api/route.js
+++ api/route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 
 const route = express.Router();
 
@@ -7,6 +7,23 @@ const route = express.Router();
 route.get('/', (req, res) =>{
     res.send('coooking')
 })
+
+route.get('/payment/:uniqueId', (req, res) =>{
+    const unique = req.params.uniqueId;
+    const manualId = req.query.manualId;
+  
+    Buyer.create({
+      manualId: manualId,
+      userId: unique
+    })
+  
+    const filePath = path.join(__dirname, 'manual files', `${manualId}.json`);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      const json = JSON.parse(data);
+      res.send(json);
+    })
+  })
+  
 
 
 
