01b4a13400797695f3821e769efefc3c4771e900
unknown <owolewaonimisi@gmail.com>
2023-05-24 20:45:15 -0700

    working

--- api/route.js
+++ api/route.js
@@ -19,10 +19,10 @@ route.get('/payment/:uniqueId', (req, res) =>{
          const json = JSON.parse(data);
          res.status(200).send(json);
 
-        //  Buyer.create({
-        //   manualId: manualId,
-        //   userId: unique
-        // })
+         Buyer.create({
+          manualId: manualId,
+          userId: unique
+         })
       }else{
         res.send('Invalid')
       }
@@ -30,5 +30,34 @@ route.get('/payment/:uniqueId', (req, res) =>{
     })
    })
   
+   route.get('/verify/:uniqueId', async(req, res) =>{
+    const unique = req.params.uniqueId;
+    const manualId = req.query.manualId;
+  
+    const filePath = path.join(__dirname, '..', 'manual files', `${manualId}.json`);
+    const verify = await Buyer.findOne({ 
+         manualId: manualId,
+         userId: unique
+    })
 
+    if(verify){
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if(data){
+           const json = JSON.parse(data);
+           res.status(200).send(json);
+        }else{
+          res.status(400).send({
+            message: 'File Error'
+          })
+        }
+  
+      })
+    }else{
+      res.status(400).send({
+        message: 'You have not paid for the '+manualId+' sunday school manual'
+      })
+    }
+
+   })
+  
 module.exports = route
