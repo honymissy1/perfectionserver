ae746ef5801674f269a76e540c98972ead8127e3
unknown <owolewaonimisi@gmail.com>
2023-06-12 18:18:31 -0700

    updated

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ const fs = require('fs');
 const path = require('path');
 const routeLink = require('./api/route')
 
-app.listen(process.env.PORT || 8080);
 app.use(cors());
+app.listen(process.env.PORT || 8080);
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', routeLink);
